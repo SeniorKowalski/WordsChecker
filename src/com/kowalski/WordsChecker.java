aae0a43a670d4d6203f856ec265480fee9d412da
Egor Laptev <laptencio@yandex.ru>
2022-11-18 15:30:53 +0400

    Refactor

--- src/com/kowalski/WordsChecker.java
+++ src/com/kowalski/WordsChecker.java
@@ -2,18 +2,18 @@ package com.kowalski;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class WordsChecker {
 
-    private String text;
+    private HashSet<String> set = new HashSet<>();
 
     public WordsChecker(String text) {
-        this.text = text;
+        List<String> words = Arrays.stream(text.split("\\P{IsAlphabetic}+")).toList();
+        this.set.addAll(words);
     }
 
     public boolean hasWord(String word) {
-        String[] words = this.text.split("\\P{IsAlphabetic}+");
-        HashSet<String> set = new HashSet<>(Arrays.stream(words).toList());
         if (set.contains(word)) {
             return true;
         }
